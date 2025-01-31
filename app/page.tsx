1e2d50e7e205474b9f6238bb2180bead6dbd4b7e
Mind Apivessa <apivessa.mind@gmail.com>
2025-01-31 10:23:49 -0500

    update next version

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Chat from '@/app/components/Chat';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-pink flex items-center justify-center p-4 w-full">
+    <main className="min-h-screen bg-black flex items-center justify-center p-4 w-full">
       <Chat />
     </main>
   );
